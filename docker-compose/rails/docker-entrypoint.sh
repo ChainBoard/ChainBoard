d1d3ca2d8ba33257d6ee7daf808cd8c492dfaedd
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-05-30 18:19:56 +0900

    fix docker-compose

--- docker-compose/rails/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ${APP_DIR}
-bundle install --path /app/vendor/bundle
-
-dockerize -wait tcp://${RAILS_DATABASE_HOST}:${RAILS_DATABASE_PORT}
-
-bundle exec rails db:create db:migrate
-bundle exec rails server -b 0.0.0.0
