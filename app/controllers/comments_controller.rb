9578bc62366ec1d41394e249dc45300dbd1ca8ee
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-01 21:02:37 +0900

    add comments controller's mock

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.last(20)
   end
 
   def show
+    @comment = Comment.includes(:comment).find_by(id: params[:id])
+    @child_comments = Comment.where(comment: @comment)
   end
 
   def create
