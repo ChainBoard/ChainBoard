077270c8a143e6cb85dc6af58600f85cc648d691
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-03 19:28:43 +0900

    add comment model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :comment, optional: true
+
+  validates :body, length: { maximum: 280 }, presence: true
+  validates :user_display_name, length: { maximum: 50 }, presence: true
+  validates :user_name, length: { maximum: 16 }, presence: true
 end
