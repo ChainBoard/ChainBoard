7d43258e64cd39f8a4813ca02f853f55f797bf9d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-03 22:06:36 +0900

    add docker-stack

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# Deploy settings
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
