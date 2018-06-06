a0ff8fa59226888f53b092d7b1ed3067e20f9810
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-06 15:16:36 +0900

    add include

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.search do
+    @comments = Comment.search(include: :comment) do
       fulltext params[:body] if params[:body].present?
       with(:user_display_name, params[:user_display_name]) if params[:user_display_name].present?
       with(:user_name, params[:user_name]) if params[:user_name].present?
