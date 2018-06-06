15fed96b66fa5e56f1a94a12b9076cbb45f32301
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-06 14:22:47 +0900

    add searchable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,10 @@ class Comment < ApplicationRecord
   validates :body, length: { maximum: 280 }, presence: true
   validates :user_display_name, length: { maximum: 50 }, presence: true
   validates :user_name, length: { maximum: 16 }, presence: true
+
+  searchable do
+    text :body
+    string :user_display_name
+    string :user_name
+  end
 end
