3f722a00740d287baff0ccd1f57739a4bba984a2
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-06-06 13:10:34 +0900

    implements controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,27 @@ class CommentsController < ApplicationController
   end
 
   def show
+    @new_comment = Comment.new
     @comment = Comment.includes(:comment).find_by(id: params[:id])
     @child_comments = Comment.where(comment: @comment)
   end
 
   def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body,
+                                    :comment_id,
+                                    :user_display_name,
+                                    :user_name)
   end
 end
