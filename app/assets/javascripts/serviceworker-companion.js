ef7fd34b0a93a0a119ef5cac73a32e064bcaf72c
marimo-nekomimi <nekomiminimoe@gmail.com>
2018-06-29 17:42:30 +0900

    add debug console log
    デバッグ用のコンソールログを追加

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,9 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+  navigator.serviceWorker.register('/assets/serviceworker.js'/*, { scope: './' }*/)
     .then(function(reg) {
       console.log('[Companion]', 'Service worker registered!');
+    }).catch(function(err) {
+      // 登録失敗 :(
+      console.log('ServiceWorker registration failed: ', err);
     });
 }
