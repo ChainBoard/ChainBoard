049f5766773fd4cf8660ca908c0b0d97cdcf69cd
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-07-04 12:43:47 +0900

    add api/comments impl

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,37 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @search_params = index_params
+    @comments = Comment.fetch_multi(search_comment_hit_ids(@search_params), includes: %i[comment comments])
+  end
+
+  def create
+    @comment = Comment.new(create_params)
+    render status: 400 unless @comment.save
+  end
+
+  private
+
+  def index_params
+    params.permit(:body,
+                  :id,
+                  :user_display_name,
+                  :user_name)
+  end
+
+  def create_params
+    params.require(:comment).permit(:body,
+                                    :comment_id,
+                                    :user_display_name,
+                                    :user_name)
+  end
+
+  def search_comment_hit_ids(query)
+    Comment.search do
+      with(:id, query[:id]) if query[:id].present?
+      fulltext query[:body] if query[:body].present?
+      with(:user_display_name, query[:user_display_name]) if query[:user_display_name].present?
+      with(:user_name, query[:user_name]) if query[:user_name].present?
+      order_by(:id, :desc)
+    end.hits.map(&:primary_key)
+  end
+end
