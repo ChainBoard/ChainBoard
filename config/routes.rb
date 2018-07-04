718e81fb2584628aa48b42f5616bbd0deb41eeaa
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-07-04 13:05:45 +0900

    remove unused endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :comments, only: %w[create show]
   root to: 'comments#index'
 
   namespace :api do
