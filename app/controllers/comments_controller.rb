b7d97062e34d7058c67325c65215112aadfea3b9
marimo-nekomimi <nekomiminimoe@gmail.com>
2018-07-13 17:47:16 +0900

    Revert "remove unused endpoint"
    This reverts commit 718e81fb2584628aa48b42f5616bbd0deb41eeaa.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,51 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  def index; end
+  def index
+    @search_params = index_params
+    @comments = Comment.fetch_multi(search_comment_hit_ids(@search_params), includes: %i[comment comments])
+
+    flash.now[:alert] = 'Comment not found.' if @comments.empty?
+  end
+
+  def show
+    @comment = Comment.fetch_by_id(params[:id], includes: %i[comment comments])
+    @new_comment = Comment.new(comment: @comment)
+
+    redirect_to root_path, alert: 'Comment not found.' unless @comment
+  end
+
+  def create
+    @comment = Comment.new(create_params)
+
+    if @comment.save
+      redirect_to @comment
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def index_params
+    params.permit(:body,
+                  :user_display_name,
+                  :user_name)
+  end
+
+  def create_params
+    params.require(:comment).permit(:body,
+                                    :comment_id,
+                                    :user_display_name,
+                                    :user_name)
+  end
+
+  def search_comment_hit_ids(query)
+    Comment.search do
+      fulltext query[:body] if query[:body].present?
+      with(:user_display_name, query[:user_display_name]) if query[:user_display_name].present?
+      with(:user_name, query[:user_name]) if query[:user_name].present?
+      order_by(:id, :desc)
+    end.hits.map(&:primary_key)
+  end
 end
