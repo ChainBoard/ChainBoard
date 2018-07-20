da60e28f99049478ebae0c73e530d5bfcc9a5fe9
marimo-nekomimi <nekomiminimoe@gmail.com>
2018-07-20 16:54:44 +0900

    puma deply settings comment out
    なんかコメントアウトしたら動いたのでそうします

--- config/puma.rb
+++ config/puma.rb
@@ -36,5 +36,5 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 plugin :tmp_restart
 
 # Deploy settings
-app_root = File.expand_path('..', __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+#app_root = File.expand_path('..', __dir__)
+#bind "unix://#{app_root}/tmp/sockets/puma.sock"
