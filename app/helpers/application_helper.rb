540295921d71482e5a44dd8964a1e7b9b53f5be4
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-07-25 08:38:02 +0900

    fix single quote garbled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
 
   def comment_body_format(body)
     raw(html_escape(body)
-            .gsub(/#([^#\p{Space}]+)/, tag.a('#\1', href: root_path(body: '\1').gsub(/%5C/, '%23\\')))
+            .gsub(/[^&]#([^#\p{Space}]+)/, tag.a('#\1', href: root_path(body: '\1').gsub(/%5C/, '%23\\')))
             .gsub(/\r\n/, tag.br))
   end
 end
